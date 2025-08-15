ef3ad5ed82b1be4f5ac7cb97c8807ca2484a28c5
glintapp-creator <epictechinfohub@gmail.com>
2025-08-16 03:15:52 +0530

    Update feed_grid.dart

--- frontend/lib/ui/feed_grid.dart
+++ frontend/lib/ui/feed_grid.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
+class FeedGrid extends StatefulWidget {
+  const FeedGrid({super.key});
+  @override State<FeedGrid> createState() => _FeedGridState();
+}
+
+class _FeedGridState extends State<FeedGrid> {
+  final dio = Dio(BaseOptions(baseUrl: 'http://localhost'));
+  List items = [];
+  String? cursor;
+  bool loading=false;
+
+  @override
+  void initState(){ super.initState(); _load(); }
+
+  Future<void> _load() async {
+    if (loading) return;
+    setState(()=>loading=true);
+    final res = await dio.get('/api/feed/public', queryParameters: { if (cursor!=null) 'cursor': cursor });
+    final data = res.data;
+    setState((){ items.addAll(data['items']); cursor = data['nextCursor']; loading=false; });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<ScrollNotification>(
+      onNotification: (sn){ if(sn.metrics.pixels>sn.metrics.maxScrollExtent-300) _load(); return false; },
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemCount: items.length,
+        itemBuilder: (_, i) {
+          final it = items[i];
+          return Image.network('http://localhost/${it['mediaKey']}', fit: BoxFit.cover);
+        },
+      ),
+    );
+  }
+}
