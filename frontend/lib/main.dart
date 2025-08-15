d2e41c4286a65d8550eb09818fe8b03489666975
glintapp-creator <epictechinfohub@gmail.com>
2025-08-16 03:14:51 +0530

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'ui/feed_grid.dart';
+import 'ui/chat_screen.dart';
 
+void main() => runApp(const GlintApp());
+
+class GlintApp extends StatelessWidget {
+  const GlintApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Glint',
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Glint')),
+      body: const FeedGrid(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.chat),
+        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const ChatScreen())),
+      ),
+    );
+  }
+}
