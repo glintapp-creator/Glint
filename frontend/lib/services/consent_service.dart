ad19035f2560eabff757e3ac764e179b7089e12e
glintapp-creator <epictechinfohub@gmail.com>
2025-08-16 03:17:11 +0530

    Update consent_service.dart

--- frontend/lib/services/consent_service.dart
+++ frontend/lib/services/consent_service.dart
@@ -1,1 +1,12 @@
+import 'package:dio/dio.dart';
 
+class ConsentService {
+  final Dio dio = Dio(BaseOptions(baseUrl:'http://localhost:8080'));
+  Future<List<dynamic>> getPolicyVersions() async {
+    final res = await dio.get('/api/policy/versions');
+    return res.data;
+  }
+  Future<void> acceptPolicies(String userId, String termsHash, String privacyHash) async {
+    await dio.post('/api/auth/accept-policies', data: { 'userId': userId, 'termsHash': termsHash, 'privacyHash': privacyHash });
+  }
+}
